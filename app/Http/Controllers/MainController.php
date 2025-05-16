c5fa7811a5639b525da3e5a7f606790c9c7eaa07
Henrique Mota <rikimotaa@gmail.com>
2025-05-15 23:58:39 -0300

    001

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,5 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class MainController extends Controller {
-    public function index($value) {
-        return view('main', ['value' => $value]);
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    echo "About Us";
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
