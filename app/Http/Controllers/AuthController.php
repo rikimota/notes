b1c940d954e9c715d87188c99d0746d90e717e08
Henrique Mota <rikimotaa@gmail.com>
2025-05-16 11:24:03 -0300

    043

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,10 @@ class AuthController extends Controller {
         return view('login');
     }
 
+    public function loginSubmit(Request $request) {
+        echo 'login submit';
+    }
+
     public function logout() {
         echo 'logout';
     }
