3e09595adb985493a4172dc0a2601a5d76ad770c
Henrique Mota <rikimotaa@gmail.com>
2025-05-19 20:26:12 -0300

    046

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,26 @@ class AuthController extends Controller {
     }
 
     public function loginSubmit(Request $request) {
-        echo 'login submit';
+        // form validation
+        $request->validate(
+            // rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            // error messages
+            [
+                'text_username.required' => 'O username e obrigatorio',
+                'text_username.email' => 'Username deve ser um email valido',
+                'text_password.required' => 'A senha e obrigatoria',
+                'text_password.min' => 'A senha deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A senha deve ter no maximo :max caracteres'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
     }
 
     public function logout() {
